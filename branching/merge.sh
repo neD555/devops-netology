8541451fd08e7941f519994cc356edc042970552
Denis <gamletspb@mail.ru>
2025-10-25 16:33:11 +0000

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,25 +2,7 @@
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
-EOFcat > branching/merge.sh <<'EOF'
-#!/bin/bash
-# display command line options
-
-count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
-EOFcat > branching/merge.sh <<'EOF'
-#!/bin/bash
-# display command line options
-
-count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
